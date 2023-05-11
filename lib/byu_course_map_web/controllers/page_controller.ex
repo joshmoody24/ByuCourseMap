2951d406b0f7629e414cb96a76b4dfb1baaca7ee
Josh Moody <joshmoody24@gmail.com>
2023-05-10 20:30:39 -0600

    customized default layout and added about page

--- lib/byu_course_map_web/controllers/page_controller.ex
+++ lib/byu_course_map_web/controllers/page_controller.ex
@@ -2,8 +2,10 @@ defmodule ByuCourseMapWeb.PageController do
   use ByuCourseMapWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
+  end
+
+  def about(conn, _params) do
+    render(conn, :about)
   end
 end
