e2ed2136b239af102aa317740f9dfdcd2030fe11
Josh Moody <joshmoody24@gmail.com>
2023-05-12 02:12:58 -0600

    updated gitignore

--- config/config.exs
+++ config/config.exs
@@ -64,9 +64,9 @@ config :phoenix, :json_library, Jason
 import_config "#{config_env()}.exs"
 
 config :crawly,
-    middlewares: [
-        {Crawly.Middlewares.UserAgent, user_agents: [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-        ]},
-        {Crawly.Pipelines.WriteToFile, folder: "/tmp", extension: "jl"}
-    ]
+  middlewares: [
+      {Crawly.Middlewares.UserAgent, user_agents: [
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+      ]},
+      {Crawly.Pipelines.WriteToFile, folder: "/tmp", extension: "jl"}
+  ]
