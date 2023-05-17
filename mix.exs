6530166f56a626b7bf6db61e61957287bbab1b1a
Josh Moody <jmoody@brevium.com>
2023-05-17 17:29:34 -0600

    changed from MySQL to Postgres

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule ByuCourseMap.MixProject do
       {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
-      {:myxql, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.16"},
